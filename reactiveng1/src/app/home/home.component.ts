993d17145d79cdaae8a48f89e117b60c3b1e64fd
nparsley <nickparsley5@gmail.com>
2021-04-29 11:10:18 -0700

    courseservice,sharereplay + coursescardlist add,refactoring

--- reactiveng1/src/app/home/home.component.ts
+++ reactiveng1/src/app/home/home.component.ts
@@ -20,7 +20,7 @@ export class HomeComponent implements OnInit {
   advancedCourses$: Observable<Course[]>;
 
 
-  constructor(private coursesService: CoursesService, private dialog: MatDialog) {
+  constructor(private coursesService: CoursesService) {
 
   }
 
@@ -29,6 +29,8 @@ export class HomeComponent implements OnInit {
     const courses$ = this.coursesService.loadAllCourses().pipe(
       map(courses => courses.sort(sortCoursesBySeqNo))
     );
+
+    courses$.subscribe(val => console.log(val));
 
     this.beginnerCourses$ = courses$.pipe(
       map(courses => courses.filter(courses => courses.category == 'BEGINNER'))
@@ -40,19 +42,7 @@ export class HomeComponent implements OnInit {
 
   }
 
-  editCourse(course: Course) {
 
-    const dialogConfig = new MatDialogConfig();
-
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = "400px";
-
-    dialogConfig.data = course;
-
-    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
-
-  }
 
 }
 
