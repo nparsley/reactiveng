11c191b3dff8d3b9f7c5dcc93955e3812179e4f2
nparsley <nickparsley5@gmail.com>
2021-05-01 12:59:02 -0700

    loadingservice,homecomponent-loading indicator reactive

--- reactiveng1/src/app/home/home.component.ts
+++ reactiveng1/src/app/home/home.component.ts
@@ -32,17 +32,24 @@ export class HomeComponent implements OnInit {
   }
 
   reloadCourses() {
+
+    // this.loadingService.loadingOn();
+
+
     const courses$ = this.coursesService.loadAllCourses().pipe(
-      map(courses => courses.sort(sortCoursesBySeqNo))
+      map(courses => courses.sort(sortCoursesBySeqNo))/* ,
+      finalize(() => this.loadingService.loadingOff()) */
     );
+
+    const loadCourses$ = this.loadingService.showLoaderUntilCompleted(courses$);
 
     courses$.subscribe(val => console.log(val));
 
-    this.beginnerCourses$ = courses$.pipe(
+    this.beginnerCourses$ = loadCourses$.pipe(
       map(courses => courses.filter(courses => courses.category == 'BEGINNER'))
     );
 
-    this.advancedCourses$ = courses$.pipe(
+    this.advancedCourses$ = loadCourses$.pipe(
       map(courses => courses.filter(courses => courses.category == 'ADVANCED'))
     );
   }
