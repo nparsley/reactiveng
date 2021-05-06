361a54a6a6c645e69663c6c3b6862f3a8e66bd82
nparsley <nickparsley5@gmail.com>
2021-05-05 17:20:04 -0700

    authstore-login,logout ui display

--- reactiveng1/src/app/app.component.ts
+++ reactiveng1/src/app/app.component.ts
@@ -1,21 +1,18 @@
 import {Component, OnInit} from '@angular/core';
 import { LoadingService } from './loading/loading.service';
 import { MessagesService } from './messages/messages.service';
+import { AuthStore } from './services/auth.store';
 
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']/* ,
-  providers: [
-    LoadingService,
-    MessagesService
-  ] */
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements  OnInit {
 
-    constructor() {
+    constructor(public auth: AuthStore) {
 
     }
 
@@ -25,7 +22,7 @@ export class AppComponent implements  OnInit {
     }
 
   logout() {
-
+    this.auth.logout();
   }
 
 }
