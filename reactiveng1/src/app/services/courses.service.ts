65c5fe15dec3186a4c4938cd3f4690003c99f71a
nparsley <nickparsley5@gmail.com>
2021-05-07 18:44:31 -0700

    coursecomponent-reactive + single data observable setup

--- reactiveng1/src/app/services/courses.service.ts
+++ reactiveng1/src/app/services/courses.service.ts
@@ -12,6 +12,27 @@ export class CoursesService {
 
   constructor(private http: HttpClient) {}
 
+  loadCourseById(courseId: number) {
+    return this.http.get<Course>(`/api/courses/${courseId}`)
+    .pipe(
+      shareReplay()
+    );
+  }
+
+
+  loadAllCourseLessons(courseId: number): Observable<Lesson[]> {
+    return this.http.get<Lesson[]>('/api/lessons', {
+      params: {
+        pageSize: "10000",
+        courseId: courseId.toString()
+      }
+    })
+    .pipe(
+      map(res => res["payload"]),
+      shareReplay()
+    );
+  }
+
   loadAllCourses(): Observable<Course[]> {
     return this.http.get<Course[]>("/api/courses").pipe(
       map(res => res["payload"]),
